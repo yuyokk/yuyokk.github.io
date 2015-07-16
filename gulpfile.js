db41cb44e6c868f3b760581d6498f1129ab939ad
Iurii Kucherov <yuyokk@gmail.com>
2015-07-16 22:41:49 +0300

    Reorganize folders structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,58 @@
 var gulp = require('gulp');
-
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var minifycss = require('gulp-minify-css');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
+var watch = require('gulp-watch');
+var shell = require('gulp-shell');
+var server = require('gulp-server-livereload');
 
-gulp.task('default', function () {
-  return gulp.src('images/**/*')
+var paths = {
+  sassEntryPoint: 'src/_sass/main.scss',
+  css: 'src/css/',
+  sass: 'src/_sass/**/*.scss',
+  images: 'src/images/**/*',
+  pages: [
+    'src/_includes/**/*.html',
+    'src/_layouts/**/*.html',
+    'src/_posts/**/*',
+    'src/*.md'
+  ]
+};
+
+gulp.task('sass', function() {
+  return gulp.src(paths.sassEntryPoint)
+    .pipe(sass())
+    .pipe(minifycss())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('images', function() {
+  return gulp.src(paths.images)
     .pipe(imagemin({
         progressive: true,
         svgoPlugins: [{ removeViewBox: false }],
         use: [pngquant()]
     }))
-    .pipe(gulp.dest('images'));
+    .pipe(gulp.dest('src/images/'));
 });
+
+gulp.task('build', shell.task(['jekyll build']));
+
+// run the server on http://localhost:8000
+gulp.task('webserver', function() {
+  gulp.src('dist')
+    .pipe(server({
+      livereload: true
+    }));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass', 'build']);
+  gulp.watch(paths.images, ['images', 'build']);
+  gulp.watch(paths.pages, ['build']);
+});
+
+gulp.task('default', ['images', 'sass', 'build', 'webserver', 'watch']);
